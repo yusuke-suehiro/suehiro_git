b197b095985516f187a2ad5483fb46815fabbeb5
yusuke-suehiro <63089903+yusuke-suehiro@users.noreply.github.com>
2020-04-10 12:16:57 +0900

    Summary
    Description

---
+++ example/example/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  example
+//
+//  Created by 末廣勇祐 on 2020/04/10.
+//  Copyright © 2020 末廣勇祐. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "hello, World!\n";
+    return 0;
+}
